3eb8199d2811937af636bef7bbc66ad3e330f3e7
LuisCrisantos <54858471+LuisCrisantos@users.noreply.github.com>
2021-11-26 00:56:50 -0800

    App de react creada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import data from './data.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+          <div >
+              <a className="brand" href="/">Genki Co.</a>
+          </div>
+          <div>
+              <a href="/cart">Carrito</a>
+              <a href="/signin">Registrarse</a>
+          </div>
       </header>
+      <main>
+          <div className="row center">
+              {
+                data.products.map(product => (
+                  <div key={product._id} className="card">
+                    <a href={`/product/${product._id}`}>
+                      <img className="medium" src={product.image} alt={product.name}/>
+                    </a>
+                    <div className="card-body">
+                        <a href={`/product/${product._id}`}>
+                          <h2>{product.name}</h2>
+                        </a>
+                        <div className="rating">
+                          <span><i className="fa fa-star"></i></span>
+                          <span><i className="fa fa-star"></i></span>
+                          <span><i className="fa fa-star"></i></span>
+                          <span><i className="fa fa-star"></i></span>
+                          <span><i className="fa fa-star"></i></span>
+                        </div>
+                        <div className="price">
+                            ${product.price} MXN
+                        </div>
+                    </div>
+                  </div>
+                ))
+              }
+                 
+          </div>
+      </main>
+      <footer className="row center">
+          Todos los derechos reservados.
+      </footer>
     </div>
   );
 }
