6f677aa513c0af72be18903f156178b21a1a3b1e
LuisCrisantos <54858471+LuisCrisantos@users.noreply.github.com>
2021-12-04 01:23:52 -0800

    Iniciar y cerrar sesión

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {BrowserRouter, Link, Route} from 'react-router-dom';
+import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const {cartItems} = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const {userInfo} = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () =>{
+    dispatch(signout());
+  }
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -20,12 +28,27 @@ function App() {
                 {cartItems.length > 0 && (
                   <span className="badge">{cartItems.length}</span>
                 )}</Link>
-                <Link to="/signin">Registrarse</Link>
+                {
+                  userInfo ? (
+                    <div className="dropdown">
+                      <Link to="#">
+                        {userInfo.name}<i className="fa fa-caret-down"></i> {' '}
+                      </Link>
+                      <ul className="dropdown-content">
+                        <Link to="#signout" onClick={signoutHandler}>
+                          Cerrar sesión
+                        </Link>
+                      </ul>
+                    </div>
+                  ) :
+                  (<Link to="/signin">Iniciar sesión</Link>)
+                }   
             </div>
         </header>
         <main>
             <Route path="/cart/:id?" component={CartScreen}></Route>
             <Route path="/product/:id" component={ProductScreen}></Route>
+            <Route path="/signin" component={SigninScreen}></Route>
             <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">
