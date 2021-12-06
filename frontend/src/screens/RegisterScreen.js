18b08042ce2877f0eb200dcc8d1e838945caf8f6
LuisCrisantos <54858471+LuisCrisantos@users.noreply.github.com>
2021-12-05 19:23:49 -0800

    Muchas cosas
    Registrar, barra de progreso, dirección de envío.

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
-import {signin} from '../actions/userActions';
+import {register} from '../actions/userActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 
-export default function SigninScreen(props) {
+export default function RegisterScreen(props) {
 
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     
     const redirect = props.location.search
     ? props.location.search.split('=')[1]
     : '/';
 
-    const userSignin = useSelector((state) => state.userSignin);
-    const {userInfo, loading, error} = userSignin;
+    const userRegister = useSelector((state) => state.userRegister);
+    const {userInfo, loading, error} = userRegister;
 
     const dispatch = useDispatch();
     const submitHandler = (e) =>{
         e.preventDefault();
-        dispatch(signin(email, password));
+        if(password !== confirmPassword){
+            alert('Las contraseñas no coinciden');
+        }else{
+            dispatch(register(name, email, password)); 
+        }  
     };
     useEffect(() =>{
         if(userInfo) {
@@ -30,9 +36,19 @@ export default function SigninScreen(props) {
     return (
         <div>
             <form className="form" onSubmit={submitHandler}>
-                <div><h1>Iniciar sesión</h1></div>
+                <div><h1>Crear cuenta</h1></div>
                 {loading && <LoadingBox></LoadingBox>}
                 {error && <MessageBox variant="danger">{error}</MessageBox>}
+                <div>
+                    <label htmlFor="name">Nombre</label>
+                    <input
+                        type="text"
+                        id="name"
+                        placeholder="Ingresar nombre"
+                        required
+                        onChange={(e) => setName(e.target.value)}
+                    ></input>
+                </div>
                 <div>
                     <label htmlFor="email">Correo electrónico</label>
                     <input
@@ -54,16 +70,27 @@ export default function SigninScreen(props) {
                     ></input>
                 </div>
                 <div>
+                    <label htmlFor="confirmPassword">Confirmar contraseña</label>
+                    <input
+                        type="password"
+                        id="confirmPassword"
+                        placeholder="Confirmar Contraseña"
+                        required
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    ></input>
+                </div>
+                <div>
                     <label/>
                     <button className="primary" type="submit">
-                        Iniciar sesión
+                        Registrarse
                     </button>
                 </div>
                 <div>
                     <label/>
                     <div>
-                        ¿Cliente nuevo? <Link to={`/register?redirect=${redirect}`}>Cree una cuenta</Link>
-                    </div>    
+                        ¿Ya tiene una cuenta? <Link to={`/signin?redirect=${redirect}`}>Iniciar sesión</Link>
+                    </div> 
+                    <label/>   
                 </div>
             </form>
         </div>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import {createStore, compose, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import {cartReducer} from './reducers/cartReducers';
 import { productListReducer, prouctDetailsReducer } from './reducers/productReducers';
-import { userSigninReducer } from './reducers/userReducer';
+import { userRegisterReducer, userSigninReducer } from './reducers/userReducer';
 
 const initialState={
     userSignin:{
@@ -13,7 +13,10 @@ const initialState={
     cart:{
         cartItems: localStorage.getItem('cartItems')
         ? JSON.parse(localStorage.getItem('cartItems'))
-        : []
+        : [],
+        shippingAddress: localStorage.getItem('shippingAddress')
+        ? JSON.parse(localStorage.getItem('shippingAddress'))
+        : {},
     }
 };
 const reducer = combineReducers({
@@ -21,6 +24,7 @@ const reducer = combineReducers({
     productDetails: prouctDetailsReducer,
     cart: cartReducer,
     userSignin: userSigninReducer,
+    userRegister: userRegisterReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
